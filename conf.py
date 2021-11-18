fa04ab07187dd7d979970b1989cd81ac1fec868c
aymeri0613 <67969274+aymeri0613@users.noreply.github.com>
2021-11-18 10:27:25 +0100

    Update conf.py

--- conf.py
+++ conf.py
@@ -46,7 +46,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'DataSud'
-copyright = u'2017, IDGO'
+copyright = u'2021, IDGO'
 author = u'IDGO'
 
 # The version info for the project you're documenting, acts as replacement for
